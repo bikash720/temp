dcae42f597099d9e0fb38285bddfceb0a9792a99
bikash720 <63512238+bikash720@users.noreply.github.com>
2024-07-18 23:46:29 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 # Change input and output details
 
-output_path="cdnm" # Output Path (Same will be pushed in main also)
-input_url="https://broken-boat-70da.wokates6127718.workers.dev/5fbe6eaec229348b9b25a391adfec2ee52f753f8616e10c51e4f8dcd54fc6b0430e2f09da85f630a0a31e905fad2073d::6acb679574225f70ee3b69d88332f8a7/1395263904/Ram.Setu.(2022).1080p.AMZN.WebRip.Hindi.264-Vegamovies.to.mkv" # Input direct file url
+output_path="test" # Output Path (Same will be pushed in main also)
+input_url="https://pixeldra.in/api/file/nnDbXhd6?download" # Input direct file url
 input_extension="mkv" # Extension of file url
 
 
@@ -14,8 +14,7 @@ mkdir $output_path
 ffmpeg -hide_banner -y -i video.$input_extension \
   -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 59 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename $output_path/360p_%03d.ts $output_path/360p.m3u8 \
   -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 59 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename $output_path/480p_%03d.ts $output_path/480p.m3u8 \
-  -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 59 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename $output_path/720p_%03d.ts $output_path/720p.m3u8 \
-  -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 59 -hls_playlist_type vod -b:v 5000k -maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename $output_path/1080p_%03d.ts $output_path/1080p.m3u8
+  -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 59 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename $output_path/720p_%03d.ts $output_path/720p.m3u8 
 
 rm video.$input_extension
 cd $output_path
@@ -27,6 +26,4 @@ echo '#EXTM3U
 #EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480
 480p.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720
-720p.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080
-1080p.m3u8' > master.m3u8
+720p.m3u8' > master.m3u8
