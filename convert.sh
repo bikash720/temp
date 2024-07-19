4b7205cb6fa19572d22805af99312e67b17ef683
bikash720 <63512238+bikash720@users.noreply.github.com>
2024-07-19 17:13:54 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 # Change input and output details
 
-output_path="Movie-Play" # Output Path (Same will be pushed in main also)
+output_path="Movie" # Output Path (Same will be pushed in main also)
 input_url="https://pixeldra.in/api/file/9mMkSUyx?download" # Input direct file url
 input_extension="mkv" # Extension of file url
 
